61a1c6efc75fe4c80098a64126a7ad08323bcee9
Frank Pinder <fpinder@comcast.net>
2019-10-19 06:37:51 -0700

    added connection to heroku

--- server.js
+++ server.js
@@ -46,8 +46,10 @@ app.set("view engine", "handlebars");
 
 
 // // Database configuration with mongoose
-// mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
-mongoose.connect("mongodb://localhost/mongoscraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper";
+
+mongoose.connect(MONGODB_URI);
+
 var db = mongoose.connection;
 
 //Show any mongoose errors
